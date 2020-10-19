79e2f659e4ca77802f0d8861298f00425c146689
jqlian1101 <jq.lian@foxmail.com>
2020-10-19 19:49:43 +0800

    ma bar

--- strategies/ma.py
+++ strategies/ma.py
@@ -1,12 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
+import pandas_datareader.data as web
+import datetime
 
 from utils import query_history
 
-history_data = query_history.QueryHistory('sh.600000',('2019-1-1', '2020-1-1'))
-df_stockload = history_data.query_from_bs()
+# df_stockload = web.DataReader("600000.SS", "yahoo", datetime.datetime(2019,1,1), datetime.datetime(2020,1,1))
 
-#绘制移动平均线图
-df_stockload['MA5'] = df_stockload.close.rolling(window=5).mean()
-df_stockload['MA20'] = df_stockload.close.rolling(window=20).mean()
-df_stockload['MA30'] = df_stockload.close.rolling(window=30).mean()
+history_data = query_history.QueryHistory('sh.600000', ('2019-1-1', '2020-1-1'))
+stock_data = history_data.query_from_bs()
+
+# print(df_stockload.index)
+# print(stock_data.index)
+
+# 分别计算5日、20日、60日的移动平均线
+ma_list = [5, 20, 60]
+
+# # 计算简单算术移动平均线MA – stock_data.close 为股票每天的收盘价
+# for ma in ma_list:
+#     stock_data['ma%d'%ma] = stock_data.close.rolling(window=ma).mean()
 
 
+# 创建fig对象
+fig = plt.figure(figsize=(8,6), dpi=100, facecolor='white')
+# 调整边框距离
+fig.subplots_adjust(left=0.09, bottom=0.10, right=0.94,top=0.9, wspace=0.2, hspace=0)
+
+
+# 创建k线图
+graph_bar = fig.add_subplot(1,1,1)
+mpf.candlestick2_ochl(graph_bar, stock_data.open, stock_data.close, stock_data.high, stock_data.low, width=0.5, colorup='r', colordown='g')
+
+# 绘制K线图
+# mpf.candlestick2_ochl(graph_bar, df_stockload.Open, df_stockload.Close, df_stockload.High, df_stockload.Low, width=0.5, colorup='r', colordown='g')  # 绘制K线走势
+
+plt.show()
--- strategies/utils/query_history.py
+++ strategies/utils/query_history.py
@@ -4,8 +4,10 @@
 # http://baostock.com/baostock/index.php/A%E8%82%A1K%E7%BA%BF%E6%95%B0%E6%8D%AE
 
 import baostock as bs
+from numpy.lib.function_base import delete
 import pandas as pd
 import os
+
 
 
 class QueryHistory:
@@ -15,25 +17,26 @@ class QueryHistory:
         self.end_date = date[1]         # 结束时间
         self.frequency = frequency      # 数据类型，默认为d，日k线；d=日k线、w=周、m=月、5=5分钟、15=15分钟、30=30分钟、60=60分钟k线数据，不区分大小写；指数没有分钟线数据；周线每周最后一个交易日才可以获取，月线每月最后一个交易日才可以获取。
         self.adjustflag = adjustflag    # 复权类型，默认不复权：3；1：后复权；2：前复权。已支持分钟线、日线、周线、月线前后复权。
+        self.bs_cols=["date","code","open","high","low","close","preclose","volume","amount","adjustflag","turn","tradestatus","pctChg","isST"]
 
     def query_from_bs(self):
-        lg = bs.login()
+        bs.login()
+        # lg = bs.login()
         # 显示登陆返回信息
-        print('login respond error_code:'+lg.error_code)
-        print('login respond  error_msg:'+lg.error_msg)
+        # print('login respond error_code:'+lg.error_code)
+        # print('login respond  error_msg:'+lg.error_msg)
 
         #### 获取沪深A股历史K线数据 ####
         # 详细指标参数，参见“历史行情指标参数”章节；“分钟线”参数与“日线”参数不同。
         # 分钟线指标：date,time,code,open,high,low,close,volume,amount,adjustflag
         # 周月线指标：date,code,open,high,low,close,volume,amount,adjustflag,turn,pctChg
-        rs = bs.query_history_k_data_plus(self.code,
-            "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST",
+        rs = bs.query_history_k_data_plus(self.code, ','.join(self.bs_cols),
             start_date=self.start_date, end_date=self.end_date,
             frequency=self.frequency, adjustflag=self.adjustflag)
 
 
-        print('query_history_k_data_plus respond error_code:' + rs.error_code)
-        print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
+        # print('query_history_k_data_plus respond error_code:' + rs.error_code)
+        # print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
 
         #### 打印结果集 ####
         data_list = []
@@ -42,6 +45,16 @@ class QueryHistory:
             data_list.append(rs.get_row_data())
 
         result = pd.DataFrame(data_list, columns=rs.fields)
+
+        # result = result.set_index('date', drop=True, append=False, inplace=False, verify_integrity=False)
+        # result.index = pd.DatetimeIndex(result.index)  # 转换为时间戳索引
+
+        result.index = pd.DatetimeIndex(result['date'])
+        del result['date']
+
+        for key in self.bs_cols:
+            if key != 'date' and key != 'code':
+                result[key] = pd.to_numeric(result[key])
 
         #### 登出系统 ####
         bs.logout()
@@ -58,6 +71,8 @@ class QueryHistory:
         df = pd.read_csv(history_dir, index_col=0)
         df['code'] = code
 
+        df.index = pd.DatetimeIndex(df.index)
+
         return df[['code','open','high','low','close','preclose','chg', 'pctChg', 'turn', 'volume', 'amount', 'tcap', 'mcap']]
 
 
@@ -65,4 +80,6 @@ class QueryHistory:
 if '__main__' == __name__:
     query_history = QueryHistory('sh.600000',('2019-1-1', '2020-1-1'))
     df = query_history.query_from_local()
-    print(df)
+
+    print(df.index)
+
