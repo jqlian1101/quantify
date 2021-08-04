31069aebf5845183b9ab9609fc7316664a73c0dd
jqlian1101 <jq.lian@foxmail.com>
2021-08-04 19:26:03 +0800

    结构调整

--- strategies/indicators/kdj.py
+++ strategies/indicators/kdj.py
@@ -1,10 +1,3 @@
-# import sys
-# import os
-
-# BASE_DIR = os.path.dirname(os.path.dirname(
-#     os.path.abspath(__file__)))  # __file__获取执行文件相对路径，整行为取上一级的上一级目录
-# sys.path.append(BASE_DIR)  # 添加路径，这个是临时的
-
 from guess_indicators import guess_indicators
 from common.get_hist_local import get_hist_data
 
